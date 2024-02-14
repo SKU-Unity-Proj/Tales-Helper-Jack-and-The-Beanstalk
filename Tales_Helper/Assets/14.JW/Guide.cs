91e4d1f34eea64d63b115ffb2d50e05061587b35
jeong3 <129027311+jeong3@users.noreply.github.com>
2024-02-14 18:53:30 +0900

    문고리 안내문구
    문고리안내문구/기존자이언트맵에서 비활성화된 맵들 렉걸려서 지워둠

---
+++ Tales_Helper/Assets/14.JW/Guide.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Guide : MonoBehaviour
+{
+    [SerializeField] private GameObject NoticeUI;
+    private bool isUIopen = false;
+    public LayerMask layerMask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckUI();
+    }
+
+    void CheckUI()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (NoticeUI.activeSelf)
+            {
+                NoticeUI.SetActive(false);
+                
+            }
+            Collider[] colliders = Physics.OverlapSphere(this.transform.position, 3f, layerMask);
+            foreach (Collider col in colliders)
+            {
+                Debug.Log(col);
+                
+                
+                if (!NoticeUI.activeSelf)
+                {
+                    NoticeUI.SetActive(true);
+                    break;
+                }
+
+                
+                
+            }
+            
+        }
+    }
+}
