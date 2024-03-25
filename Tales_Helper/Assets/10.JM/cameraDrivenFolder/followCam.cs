318b46f64323d0b94ba0e00c8cc97e331f72176c
이정민 <79199117+daniellee1019@users.noreply.github.com>
2024-03-25 22:25:43 +0900

    현관 상황 연출 작업
    파쿠르 추가

--- Tales_Helper/Assets/10.JM/cameraDrivenFolder/followCam.cs
+++ Tales_Helper/Assets/10.JM/cameraDrivenFolder/followCam.cs
@@ -7,15 +7,14 @@ public class followCam : MonoBehaviour
 
     public Transform player; // 플레이어 Transform
     public float xOffset = 0f; // X축 오프셋
-    public float yOffset = 0f; // Y축 오프셋
 
     private Vector3 followPosition;
 
     void Update()
     {
-        // 플레이어의 Z 위치를 따라가되, X와 Y 위치는 고정
+        // 플레이어의 Y, Z 위치를 따라가되, X 위치는 고정
         followPosition.x = xOffset; // X축은 고정 오프셋 사용
-        followPosition.y = yOffset; // Y축은 고정 오프셋 사용
+        followPosition.y = player.position.y;
         followPosition.z = player.position.z;
 
         transform.position = followPosition;
