8a4fa0cf91c21e369ca077c55da0e34d9d8c2ad0
이정민 <79199117+daniellee1019@users.noreply.github.com>
2024-03-26 10:43:49 +0900

    잭 모델 리깅 수정
     머리 빵구 매꿈

---
+++ Tales_Helper/Assets/10.JM/BTsources/BasicManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicManager : MonoBehaviour
+{
+    public static BasicManager Instance { get; private set; }
+    public DetectableTarget PlayerTarget { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void RegisterPlayer(DetectableTarget playerTarget)
+    {
+        PlayerTarget = playerTarget;
+    }
+}
