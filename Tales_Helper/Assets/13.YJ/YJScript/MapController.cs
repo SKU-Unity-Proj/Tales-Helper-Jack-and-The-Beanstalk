51f51d33211500cbdca2eeab506413b6aaf5d5d6
20190965YoungJin <dudwls1564954@naver.com>
2024-04-12 22:01:28 +0900

    미니맵 컨트롤러

--- Tales_Helper/Assets/13.YJ/YJScript/MapController.cs
+++ Tales_Helper/Assets/13.YJ/YJScript/MapController.cs
@@ -1,22 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapController : MonoBehaviour
 {
     public GameObject mapUI;
+    public Image mapSignature;
+    public Image[] miniMap;
+    public Sprite[] miniMapType;
+    public Sprite[] mapState;
+    private int currentIndex = 0;
 
-    void Start()
+    #region 싱글톤 패턴
+    private static MapController instance = null;
+    void Awake()
     {
-
+        if (null == instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+            Destroy(this.gameObject);
     }
+    public static MapController Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+    #endregion
 
     void Update()
     {
-        OpenMapUI();
+        OpenCloseMap();
+        CurrentRoom();
     }
 
-    void OpenMapUI()
+    public void ChangeImage(int newIndex)
+    {
+        currentIndex = newIndex;
+
+
+    }
+
+    void CurrentRoom() // 현재 위치한 방의 색상 변경
+    {
+        mapSignature.sprite = mapState[currentIndex];
+    }
+
+    void OpenCloseMap()
     {
         if(Input.GetKeyDown(KeyCode.M))
         {
