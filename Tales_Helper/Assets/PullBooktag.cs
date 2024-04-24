b0c1a9debb03d300c82752711a4ada35fa726dea
jeong3 <129027311+jeong3@users.noreply.github.com>
2024-04-24 18:59:33 +0900

    book2
    이미지 변환, 상호작용 키 눌렀을 때
    이미지 크게 보이는거 해야함

--- Tales_Helper/Assets/PullBooktag.cs
+++ Tales_Helper/Assets/PullBooktag.cs
@@ -6,10 +6,14 @@ public class PullBooktag : MonoBehaviour
 {
     public Animator playerAnimator; // 플레이어 애니메이터
     public Animator bookAnimator; // 책 애니메이터
+    public Animator playerAnimator2; // 플레이어 애니메이터
+    public Animator bookAnimator2; // 책 애니메이터
     public Transform targetPos; // F을 눌렀을 때 위치
+    public Transform targetPos2; // F을 눌렀을 때 위치
     public float interactionDistance = 1f; // 상호작용 가능한 최대 거리
     public KeyCode interactionKey = KeyCode.F; // 상호작용 키
     public GameObject paperObject;
+    public GameObject paperObject2;
 
     // Update is called once per frame
     void Update()
@@ -17,6 +21,7 @@ public class PullBooktag : MonoBehaviour
         if (Input.GetKeyDown(interactionKey))
         {
             CheckBook();
+            CheckBook2();
         }
     }
 
@@ -53,6 +58,39 @@ public class PullBooktag : MonoBehaviour
             Debug.LogError("Paper 오브젝트가 할당되지 않았습니다.");
         }
     }
+    private void CheckBook2()
+    {
+        // 플레이어 위치에서 가장 가까운 버튼 찾기 (버튼 오브젝트는 "Book" 태그로 지정되어 있어야 함)
+        GameObject nearestBook = FindNearestWithTag("Book2");
+
+        if (nearestBook != null)
+        {
+            // 플레이어와 버튼 간의 거리 계산
+            float distance = Vector3.Distance(transform.position, nearestBook.transform.position);
+            // 거리가 interactionDistance 이내인지 확인
+            if (distance <= interactionDistance)
+            {
+                SetPositionAndRotation2();
+                // 플레이어와 버튼의 애니메이션 재생
+                playerAnimator2.SetTrigger("PullBook");
+                bookAnimator2.SetTrigger("PullBook");
+                Invoke("ActivatePaper2", 3.2f);
+            }
+        }
+    }
+
+    public void ActivatePaper2()
+    {
+        // Paper 오브젝트가 존재하면 활성화
+        if (paperObject2 != null)
+        {
+            paperObject2.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Paper 오브젝트가 할당되지 않았습니다.");
+        }
+    }
 
     public void SetPositionAndRotation()
     {
@@ -60,6 +98,18 @@ public class PullBooktag : MonoBehaviour
         {
             transform.position = targetPos.position; // 위치 설정
             transform.rotation = targetPos.rotation; // 회전 설정
+        }
+        else
+        {
+            Debug.LogError("Target position is not assigned!");
+        }
+    }
+    public void SetPositionAndRotation2()
+    {
+        if (targetPos2 != null)
+        {
+            transform.position = targetPos2.position; // 위치 설정
+            transform.rotation = targetPos2.rotation; // 회전 설정
         }
         else
         {
