2c7aafdd41693407df599741b24a2fe2cb72d796
20190965YoungJin <dudwls1564954@naver.com>
2024-06-07 16:54:51 +0900

    카메라 수정,

--- Tales_Helper/Assets/13.YJ/YJScript/Sentis/ShowSentisScreen.cs
+++ Tales_Helper/Assets/13.YJ/YJScript/Sentis/ShowSentisScreen.cs
@@ -17,39 +17,56 @@ public class ShowSentisScreen : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        playerIn = true;
+        if (other.CompareTag("Player")) // 플레이어인지 확인 (필요한 경우)
+        {
+            playerIn = true;
+        }
     }
+
     private void OnTriggerExit(Collider other)
     {
-        playerIn = false;
+        if (other.CompareTag("Player")) // 플레이어인지 확인 (필요한 경우)
+        {
+            playerIn = false;
+            HideScreen(); // 트리거 박스를 벗어났을 때 초기화
+        }
     }
 
     private void Update()
     {
         if (playerIn)
         {
-            if(!isShow)
+            if (!isShow)
             {
                 if (Input.GetKeyDown(KeyCode.F))
                 {
-                    screenCam.Priority = 11;
-                    Cursor.lockState = CursorLockMode.None;
-                    Cursor.visible = true;
-
-                    Cursor.SetCursor(circleCursor, new Vector2(16, 16), CursorMode.Auto);
-                    isShow = true;
+                    ShowScreen();
                 }
-                return;
             }
-
-            if (Input.GetKeyDown(KeyCode.F) && isShow)
+            else
             {
-                screenCam.Priority = 2;
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-
-                isShow = false;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    HideScreen();
+                }
             }
         }
+    }
+
+    private void ShowScreen()
+    {
+        screenCam.Priority = 11;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Cursor.SetCursor(circleCursor, new Vector2(16, 16), CursorMode.Auto);
+        isShow = true;
+    }
+
+    private void HideScreen()
+    {
+        screenCam.Priority = 2;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isShow = false;
     }
 }
